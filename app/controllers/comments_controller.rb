771a130cb0b708bcc3281793871ffcd7a5d4f215
nicole <nmorales@trinity7designs.com>
2014-11-30 15:13:29 -0500

    Updating controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,47 +1,50 @@
-class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  respond_to :html
+ class CommentsController < ApplicationController
+ before_action :set_pin, only: [:show, :edit, :update, :destroy]
 
   def index
     @comments = Comment.all
-    respond_with(@comments)
   end
 
   def show
-    respond_with(@comment)
   end
 
   def new
     @comment = Comment.new
-    respond_with(@comment)
   end
 
   def edit
   end
 
   def create
-    @comment = Comment.new(comment_params)
-    @comment.save
-    respond_with(@comment)
+    @comment = Comment.new(pin_params)
+    if @comment.save
+      redirect_to @comment, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
   end
 
   def update
-    @comment.update(comment_params)
-    respond_with(@comment)
+    if @comment.update(comment_params)
+      redirect_to @comment, notice: 'Pin was successfully updated.'
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
     @comment.destroy
-    respond_with(@comment)
+    redirect_to comment_url
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_comment
       @comment = Comment.find(params[:id])
     end
 
-    def comment_params
-      params.require(:comment).permit(:description)
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pin_params
+      params.require(:pin).permit(:description)
     end
 end
